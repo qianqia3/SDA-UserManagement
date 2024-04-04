1e8615613c837bc7f25d4a6319a6205f5e85471a
laauuurrrraaaaa <qianqia3@andrew.cmu.edu>
2024-04-03 17:25:48 -0700

    Add .gitignore file

--- db.py
+++ db.py
@@ -4,5 +4,5 @@ from flask_pymongo import pymongo
 
 CONNECTION_STRING = "mongodb+srv://qianqia3:1orange@sda.oh22dpu.mongodb.net/?retryWrites=true&w=majority&appName=SDA"
 client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database("User")
+db = client.get_database("Users")
 user_collection = pymongo.collection.Collection(db, "Users")
